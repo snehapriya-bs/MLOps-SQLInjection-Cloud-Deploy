0faf3287a04b90a15a4714ae7e37c557bd68249a
snehapriya-bs <152855542+snehapriya-bs@users.noreply.github.com>
2025-03-22 11:30:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import uvicorn
 from app.routes import router
 
 app = FastAPI()
@@ -7,8 +9,11 @@ app = FastAPI()
 app.include_router(router)
 
 def main():
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8080))  # ✅ Default to 8080 for Cloud Run
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
 
 if __name__ == "__main__":
     main()
+
+
+
