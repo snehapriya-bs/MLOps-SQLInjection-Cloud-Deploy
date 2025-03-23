62e9858a928399f52732078fdf5f3b8ec789f953
snehapriya-bs <152855542+snehapriya-bs@users.noreply.github.com>
2025-03-23 20:38:13 +0530

    Update config.py

--- sql_injection/config.py
+++ sql_injection/config.py
@@ -6,6 +6,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = os.path.join(BASE_DIR, "..", "data", "sql_injection_dataset.csv")  # Move one level up
 MODEL_PATH = os.path.join(BASE_DIR, "trained_model", "model.pkl")
 VECTORIZER_PATH = os.path.join(BASE_DIR,"trained_model", "vectorizer.pkl")
+VOCAB_PATH = os.path.join(BASE_DIR,"trained_model", "vocab.pkl")
 
 API_VERSION = "1.0.0"
 PROJECT_NAME = "SQL Injection Detector"
