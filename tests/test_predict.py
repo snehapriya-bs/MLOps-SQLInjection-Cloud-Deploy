67ef84f9c9a67d310c26f0f6d618c932efa2b9f2
snehapriya-bs <152855542+snehapriya-bs@users.noreply.github.com>
2025-03-26 13:45:52 +0530

    Update test_predict.py

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -27,8 +27,8 @@ def test_safe_sql(sample_sql_queries):
     assert result[0]["prediction"] == "Safe"
 
 def test_sql_injection(sample_sql_queries):
-    result = predict_from_sql_file("tests/test_data/sql_injection.sql")
-    assert result[0]["prediction"] == "SQL Injection"
+    result_sql = predict_from_sql_file("tests/test_data/sql_injection.sql")
+    assert result_sql[0]["prediction"] == "SQL Injection"
 
 if __name__ == "__main__":
     pytest.main()
